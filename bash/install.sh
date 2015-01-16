b977d6f036ec1605a57992c0505227b5e5fab7e4
EricBouwers <EricBouwers@gmail.com>
2015-01-16 12:06:29 +0100

    Small tweak, making delete key work in terminal

--- bash/install.sh
+++ bash/install.sh
@@ -14,8 +14,8 @@ then
   bash_path=$(brew --prefix)/bin/bash 
   if [ -x $bash_path ] && [ $SHELL != "$bash_path" ]
   then
-    echo -e "  Found homebrew bash, setting this one as default,\n"
-    echo -e "  you probably need to type your password twice.\n"
+    echo -e "\n  Found homebrew bash, setting this one as default,\n"
+    echo -e "\n  you probably need to type your password twice.\n"
     sudo bash -c "echo $bash_path >> /private/etc/shells"
     chsh -s $bash_path 
   fi
